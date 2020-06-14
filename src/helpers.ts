c6f62cfe83441c8f65b05f019b0efbfa58afe1a4
Esau Murillo <emurillo@artear.com>
2020-06-14 20:41:25 -0300

    added create context wrapper for undefined checking

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export function api<T>(url: string, options: Object = {}): Promise<T> {
   return fetch(url, options)
     .then(response => {
@@ -10,3 +12,14 @@ export function api<T>(url: string, options: Object = {}): Promise<T> {
       throw error /* <-- rethrow the error so consumer can catch it */
     })
 };
+
+// https://www.carlrippon.com/react-context-with-typescript-p4/
+export function createCtx<ContextType>() {
+  const ctx = React.createContext<ContextType | undefined>(undefined);
+  function useCtx() {
+    const c = React.useContext(ctx);
+    if (!c) throw new Error("useCtx must be inside a Provider with a value");
+    return c;
+  }
+  return [useCtx, ctx.Provider] as const;
+}
