0d4abb9cdff3eb750f25dd63f2207e4917def30b
Esau Murillo <emurillo@artear.com>
2020-06-15 04:19:33 -0300

    get only the necessary values without the timestamp ones

--- src/context/Player/reducer.ts
+++ src/context/Player/reducer.ts
@@ -25,7 +25,7 @@ export default function reducer(state = initialState, action: PlayerActionTypes)
     case LOAD_CARDS:
       return {
         ...state,
-        cards: action.payload
+        cards: action.payload.map(({ id, effect, value }) => ({ id, effect, value }))
       }
     default:
       return state;
