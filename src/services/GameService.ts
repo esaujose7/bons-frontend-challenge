50e7d3dc543615db657f26ca4c2e8e5e1fcf30a9
Esau Murillo <emurillo@artear.com>
2020-06-14 21:17:51 -0300

    pass content type application json on the game service to the api

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -5,7 +5,8 @@ class GameService {
   static async start(playerName: string) {
     return api<GameEntity>(process.env.REACT_APP_BONS_BASE_URL + '/games', {
       method: 'POST',
-      body: JSON.stringify({ name: playerName })
+      body: JSON.stringify({ name: playerName }),
+      headers: { 'Content-Type': 'application/json' }
     });
   }
 }
