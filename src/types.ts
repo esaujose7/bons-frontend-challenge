00ebe83546007412f30a99706ca1efd55b12bdf7
Esau Murillo <emurillo@artear.com>
2020-06-14 22:07:51 -0300

    typing

--- src/types.ts
+++ src/types.ts
@@ -3,12 +3,40 @@ export type GameResult = 'WIN' | 'LOSE';
 export type GameStatus = 'IDLE' | 'ONGOING' | GameResult;
 
 export interface GameEntity {
-  id: string | null;
-  currentTurn: number | null;
-  maxTurns: number | null;
-  turnsLeft: number | null;
+  id: string;
+  currentTurn: number;
+  maxTurns: number;
+  turnsLeft: number;
 }
 
 export interface GameState extends GameEntity {
   status: GameStatus;
 };
+
+export interface PlayerEntity {
+  id: string;
+  hp: number;
+  maxHp: number;
+  shield: number;
+  name: string;
+  cards: Card[];
+};
+
+export interface MonsterEntity {
+  id: string;
+  hp: number;
+  maxHp: number;
+  shield: number;
+  name: string;
+  image: string;
+}
+
+export interface Card {
+  id: string;
+  value: number;
+  effect: PlayerEffects;
+};
+
+export type PlayerEffects = 'HEAL' | 'SHIELD' | 'DAMAGE';
+
+export type MonsterEffects = PlayerEffects | 'HORROR';
