cd20fe442f9c9fe974e3ff7bc2444df6ede5241e
Esau Murillo <emurillo@artear.com>
2020-06-16 00:26:12 -0300

    clearer App declaration

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,27 @@ import { useGameState } from './context/Game';
 import { PlayerContextProvider } from './context/Player';
 import { MonsterContextProvider } from './context/Monster';
 
-import { IDLE, ONGOING, WIN, LOSE } from './types';
+import { IDLE, ONGOING } from './types';
 
 import './App.scss';
 
 const App: React.FC = () => {
   const { id: gameId, currentTurn, status: gameStatus } = useGameState();
 
-  switch(gameStatus) {
-    case IDLE:
-    default:
-      return <Login />;
-    case ONGOING:
-      return (
-        <PlayerContextProvider gameId={gameId} currentTurn={currentTurn}>
-          <MonsterContextProvider gameId={gameId} currentTurn={currentTurn}>  
-            <GameBoard />
-          </MonsterContextProvider>
-        </PlayerContextProvider>
-      );
-    case WIN:
-      return <EndGameModal result={WIN} />;
-    case LOSE:
-      return <EndGameModal result={LOSE} />;
+  if (gameStatus === IDLE) {
+    return <Login />;
   }
+
+  return (
+    <>
+      {gameStatus !== ONGOING && <EndGameModal result={gameStatus} />}
+      <PlayerContextProvider gameId={gameId} currentTurn={currentTurn}>
+        <MonsterContextProvider gameId={gameId} currentTurn={currentTurn}>  
+          <GameBoard />
+        </MonsterContextProvider>
+      </PlayerContextProvider>
+    </>
+  );
 }
 
 export default App;
