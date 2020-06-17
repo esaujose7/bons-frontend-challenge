649bb710009b541d67d7b80e5e895232894a19d3
Esau Murillo <emurillo@artear.com>
2020-06-17 00:44:05 -0300

    modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import './styles.scss';
 
-const Modal: React.FC = () => {
-  return (
-    <div>
-      
+const Modal: React.FC = ({ children }) => (
+  <div className="modal is-active">
+    <div className="modal-background"></div>
+    <div className="modal-card">
+      <section className="modal-card-body center-content" style={{ display: 'flex' }}>
+        {children}
+      </section>
     </div>
-  )
-}
+  </div>
+);
 
-export default Modal
+export default Modal;
