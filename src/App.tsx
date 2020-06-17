387933621897c446c42c3cafd20b353463bde850
Esau Murillo <emurillo@artear.com>
2020-06-17 01:47:38 -0300

    implementing error game modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,31 @@ import React from 'react';
 import Login from './features/Login';
 import GameBoard from './features/GameBoard';
 import EndGameModal from './features/EndGameModal';
+import ErrorGameModal from './features/ErrorGameModal';
 
 import { useGameState } from './context/Game';
 import { PlayerContextProvider } from './context/Player';
 import { MonsterContextProvider } from './context/Monster';
 
-import { IDLE, ONGOING } from './types';
+import { IDLE, ONGOING, ERROR } from './types';
 
 import './App.scss';
 
 const App: React.FC = () => {
   const { id: gameId, currentTurn, status: gameStatus } = useGameState();
 
+  if (gameStatus === ERROR) {
+    return <ErrorGameModal />
+  }
+
   if (gameStatus === IDLE) {
-    return <Login />;
+    return (
+      <div className="container">
+        <h1>Welcome to Bons Game!</h1>
+        <h2>What's your name?</h2>
+        <Login />
+      </div>
+    );
   }
 
   return (
