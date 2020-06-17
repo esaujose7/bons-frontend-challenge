2579a60af8acd37c8cda54ad454b4f1994c20540
Esau Murillo <emurillo@artear.com>
2020-06-17 01:49:27 -0300

    Monster output message

--- src/features/GameBoard/Monster/Monster.tsx
+++ src/features/GameBoard/Monster/Monster.tsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import ParticipantInfo from '../../../components/ParticipantInfo';
 import { useMonsterState } from '../../../context/Monster';
+import { MonsterEffectsEntity } from '../../../types';
 
-const Enemy = () => {
+type Props = {
+  lastMonsterEffect: MonsterEffectsEntity | null
+}
+
+const outputReadableMonsterEffectMessage = (monsterName: string, monsterEffect: MonsterEffectsEntity) => {
+  const monsterMessagesDict = {
+    HEAL: `${monsterName} healed himself for an amount of ${monsterEffect.value} points.`,
+    SHIELD: `${monsterName} shielded himself for an amount of ${monsterEffect.value} points.`,
+    DAMAGE: `${monsterName} damaged you for an amount of ${monsterEffect.value} points. Be sure to heal or shield up!`,
+    HORROR: `${monsterName} just horrored you!`
+  };
+  return monsterMessagesDict[monsterEffect.effect];
+};
+
+const Monster: React.FC<Props> = ({ lastMonsterEffect }) => {
   const { name, hp, maxHp, shield, image } = useMonsterState();
 
   return (
-    <ParticipantInfo name={name} hp={hp} maxHp={maxHp} shield={shield} image={image} />
+    <ParticipantInfo name={name} hp={hp} maxHp={maxHp} shield={shield} image={image}>
+      {lastMonsterEffect && (
+        <p>
+          {outputReadableMonsterEffectMessage(name, lastMonsterEffect)}
+        </p>
+      )}
+    </ParticipantInfo>
   );
 };
 
-export default Enemy
+export default Monster
