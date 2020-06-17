91e9f5ee114e22ef3c0213d714d04793048e21e5
Esau Murillo <emurillo@artear.com>
2020-06-16 23:31:13 -0300

    removing useless state and cleaning up the gameboard

--- src/features/GameBoard/Gameboard.tsx
+++ src/features/GameBoard/Gameboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Enemy from './Enemy';
+import Monster from './Monster';
 import Player from './Player';
 import Cards from './Cards';
 import Turns from './Turns';
@@ -8,13 +8,9 @@ import { Card } from '../../types';
 import useGameContext  from '../../context/Game';
 
 const Gameboard: React.FC = () => {
-  const { state: { currentTurn, turnsLeft, lastMonsterEffect, maxTurns }, actions: { nextTurn } } = useGameContext();
+  const { state: { currentTurn, lastMonsterEffect }, actions: { nextTurn } } = useGameContext();
   const [card, setCard] = React.useState<Card | null>(null);
   const isLastMonsterEffectHorror = lastMonsterEffect && lastMonsterEffect.effect === 'HORROR';
-
-  const endTurn = () => {
-    isLastMonsterEffectHorror ? nextTurn(undefined) : nextTurn(card?.id) 
-  };
 
   React.useEffect(() => {
     setCard(null);
@@ -23,12 +19,12 @@ const Gameboard: React.FC = () => {
   return (
     <div className="container columns mt-4">
       <div className="gameboard-info column is-offset-2 is-6">
-        <Enemy />
+        <Monster />
         <Player />
         <Cards selectedCard={card} selectCard={isLastMonsterEffectHorror ? () => {} : setCard} />
       </div>
-      <Turns endTurn={endTurn} />
-      {isLastMonsterEffectHorror && "You've been horrified! Can't choose a card :("}
+      <Turns endTurn={() => { nextTurn(card?.id); }} />
+      {isLastMonsterEffectHorror && "You've been horrified! Can't choose a card for this turn :("}
     </div>
   );
 }
