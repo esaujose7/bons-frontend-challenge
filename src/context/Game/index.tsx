b19055bcde818849a82064dbda92e2d76c673451
Esau Murillo <emurillo@artear.com>
2020-06-18 00:12:55 -0300

    implement isLoading on GameContext

--- src/context/Game/index.tsx
+++ src/context/Game/index.tsx
@@ -1,33 +1,44 @@
-import React, { useReducer, useEffect, useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import reducer, { initialState } from './reducer';
 import GameService from '../../services/GameService';
 import { GAME_START, PLAY_TURN, GAME_WON, GAME_LOST, RESTART_GAME, GAME_ERROR, GAME_LOAD_AFTER_ERROR, GameContextType } from './types';
-import { ONGOING } from '../../types';
 import { createCtx } from '../../utilities';
 
 const [useGameContext, Provider] = createCtx<GameContextType>();
 
 const GameContextProvider: React.FC = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [isLoading, setIsLoading] = useState(false);
   const [initialLoadError, setInitialLoadError] = useState<Error | null>(null);
 
   const startGame = (playerName: string): void => {
+    setIsLoading(true);
     setInitialLoadError(null);
     GameService.start(playerName)
       .then(data => dispatch({ type: GAME_START, payload: data }))
-      .catch(setInitialLoadError);
+      .catch(setInitialLoadError)
+      .finally(() => { setIsLoading(false); });
+  };
+
+  const nextTurn = (cardId: string | undefined): void => {
+    setIsLoading(true);
+    GameService.playNextTurn(state.id, cardId)
+      .then(data => dispatch({ type: PLAY_TURN, payload: data }))
+      .catch(notifyError)
+      .finally(() => { setIsLoading(false); });
+  };
+
+  const loadGameAfterError = () => {
+    setIsLoading(true);
+    GameService.getById(state.id)
+      .then(data => dispatch({ type: GAME_LOAD_AFTER_ERROR, payload: data }))
+      .catch(err => { setInitialLoadError(err); restartGame(); })
+      .finally(() => { setIsLoading(false); });
   };
 
   const notifyError = (err: Error) => {
     dispatch({ type: GAME_ERROR, payload: err });
   }
-
-  const nextTurn = (cardId: string | undefined): void => {
-    const gameId = state.id;
-    GameService.playNextTurn(gameId, cardId)
-      .then(data => dispatch({ type: PLAY_TURN, payload: data }))
-      .catch(notifyError);
-  };
 
   const notifyGameIsWon = (): void => {
     dispatch({ type: GAME_WON });
@@ -41,20 +52,13 @@ const GameContextProvider: React.FC = ({ children }) => {
     dispatch({ type: RESTART_GAME });
   }
 
-  const loadGameAfterError = () => {
-    GameService.getById(state.id)
-      .then(data => dispatch({ type: GAME_LOAD_AFTER_ERROR, payload: data }))
-      .catch(setInitialLoadError);
+  const contextOutput = {
+    state: { ...state, isLoading },
+    actions: { startGame, nextTurn, notifyGameIsWon, notifyGameIsLost, restartGame, notifyError, loadGameAfterError }
   };
 
-  useEffect(() => { // if the game is ongoing and we run out of turns it means we lost.
-    if (state.currentTurn === state.maxTurns && state.status === ONGOING) {
-      notifyGameIsLost();
-    }
-  }, [state.currentTurn, state.maxTurns, state.status]);
-
   return (
-    <Provider value={{ state, actions: { startGame, nextTurn, notifyGameIsWon, notifyGameIsLost, restartGame, notifyError, loadGameAfterError } }}>
+    <Provider value={contextOutput}>
       {children}
       {initialLoadError && 'Something went wrong. Please try again.'}
     </Provider>
