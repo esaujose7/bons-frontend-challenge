7ccdf4c0afc830b4c4ede64fd71c98236957748f
Esau Murillo <emurillo@artear.com>
2020-06-18 01:30:35 -0300

    turns styling

--- src/features/GameBoard/Turns/Turns.tsx
+++ src/features/GameBoard/Turns/Turns.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { useGameState } from '../../../context/Game';
+import './style.scss';
 
-const Turns: React.FC<{ endTurn: Function }> = ({ endTurn }) => {
+const Turns: React.FC<{ endTurn: Function, disabled: boolean }> = ({ endTurn, disabled }) => {
 
   const { currentTurn, maxTurns, turnsLeft } = useGameState();
 
   return (
-      <div className="gameboard-turns column is-offset-2 is3">
-        <h2>Turns</h2>
-        <div>
-          CURRENT: <br />
-          {currentTurn + 1 > maxTurns ? maxTurns : currentTurn + 1}
+      <div className="gameboard-turns column is3 flex-center mt-6">
+        <h2 className="mb-3">Turns</h2>
+        <div className="flex-center mb-2">
+          <h3>CURRENT:</h3>
+          <span>{currentTurn + 1 > maxTurns ? maxTurns : currentTurn + 1}</span>
         </div>
-        <div>
-          PAST: <br />
-          {currentTurn}
+        <div className="flex-center mb-2">
+          <h3>PAST:</h3>
+          <span>{currentTurn}</span>
         </div>
-        <div>
-          LEFT: <br />
-          {turnsLeft}
+        <div className="flex-center mb-2">
+          <h3>LEFT:</h3>
+          <span>{turnsLeft}</span>
         </div>
-        <button onClick={() => { endTurn(); }} className="button is-primary">
-          END TURN.
+        <button onClick={() => { endTurn(); }} className="button is-primary" disabled={disabled}>
+          END TURN
         </button>
       </div>
   );
