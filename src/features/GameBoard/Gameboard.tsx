c09b488436fbeda5b8e45c1bf4b6a1c225c9bc48
Esau Murillo <emurillo@artear.com>
2020-06-18 00:11:55 -0300

    let the gameboard itself decide if we win or lose

--- src/features/GameBoard/Gameboard.tsx
+++ src/features/GameBoard/Gameboard.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Monster from './Monster';
 import Player from './Player';
 import Cards from './Cards';
 import Turns from './Turns';
-import { Card } from '../../types';
+import { Card, ONGOING } from '../../types';
 
 import useGameContext  from '../../context/Game';
+import { usePlayerState } from '../../context/Player';
+import { useMonsterState } from '../../context/Monster';
 
 const Gameboard: React.FC = () => {
-  const { state: { currentTurn, lastMonsterEffect }, actions: { nextTurn } } = useGameContext();
-  const [card, setCard] = React.useState<Card | null>(null);
+  const { state: { currentTurn, maxTurns, lastMonsterEffect, id: gameId, status }, actions: { nextTurn, notifyGameIsLost, notifyGameIsWon } } = useGameContext();
   const isLastMonsterEffectHorror = lastMonsterEffect && lastMonsterEffect.effect === 'HORROR';
 
-  React.useEffect(() => {
+  const [card, setCard] = useState<Card | null>(null);
+  const { hp: playerHp, id: playerId } = usePlayerState();
+  const { hp: monsterHp, id: monsterId } = useMonsterState();
+
+  useEffect(() => {
+    if (status === ONGOING && monsterId !== '' && monsterHp <= 0) { // monster died, we won
+      notifyGameIsWon();
+    }
+  }, [monsterHp]);
+
+  useEffect(() => {
+    if (status === ONGOING && playerId !== '' && playerHp <= 0) {// we died
+      notifyGameIsLost();
+    }
+  }, [playerHp]);
+
+  useEffect(() => {
+    if (status === ONGOING && gameId !== '' && currentTurn === maxTurns) { // we ran out of turns
+      notifyGameIsLost();
+    }
+  }, [currentTurn, maxTurns]);
+
+  useEffect(() => {
     setCard(null);
   }, [currentTurn]);
 
